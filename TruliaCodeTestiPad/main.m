b5add01e557ab5b13129d8b11fd4b51e0d22334a
Neha Shevade <nshevade@trulia.com>
2013-09-11 10:20:10 -0700

    Initial Commit

---
+++ TruliaCodeTestiPad/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TruliaCodeTestiPad
+//
+//  Created by Neha Shevade on 9/11/13.
+//  Copyright (c) 2013 Trulia, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TLBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TLBAppDelegate class]));
+    }
+}
